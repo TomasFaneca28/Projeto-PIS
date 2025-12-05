880fbafb2b51b814584264c3b3bd158fc16392fd
TomasFaneca28 <tomasbritofaneca@gmail.com>
2025-12-05 20:32:25 +0000

    Organizacao + Login&Registo

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+//const routes = require('./src/routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','html/login.html'));
+});
+app.get('/registo', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public','html/registo.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor a correr na porta ${PORT}`);
+});
+
+module.exports = app;
