3652574b7a31ecbe835909e45e786f3dc4796cd0
DiogoDuarte-git <diogojrduarte@gmail.com>
2026-01-09 18:52:03 +0000

    logo

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql2');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '123456789',
+  password: '',
   database: 'projetopis',
   waitForConnections: true,
   connectionLimit: 10,
